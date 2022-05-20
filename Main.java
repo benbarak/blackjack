c73a11877ad338201987e423590c94b2f341c8fb
benbarak <71935599+benbarak@users.noreply.github.com>
2022-05-20 15:24:06 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     static int pot = 3000;
-    static ArrayList<Card> hand;
+    static ArrayList<Card> hand = new ArrayList<>();
+    static ArrayList<Card> dealerHand = new ArrayList<>();
     static int bet = 0;
     static boolean burn = false;
+    static boolean dealerBurn = false;
+    static int deckIndex = 0;
+    static ArrayList<Card> deck = new ArrayList<>();
+    static boolean blackjack = false;
 
     public static void main(String[] args){
-        ArrayList<Card> deck = new ArrayList<>();
         for(Suit suit : Suit.values()){
             for(Rank rank : Rank.values()){
-                deck.add(new Card(suit, rank));
+                deck.add(new Card(suit, rank, false));
             }
         }
 
@@ -26,6 +30,7 @@ public class Main {
             play();
             dealerPlay();
             result();
+            clear();
         }
     }
 
@@ -36,10 +41,12 @@ public class Main {
     private static void placeBet(){
         System.out.println("Place your Bet");
         int temp = scanner.nextInt();
-        while(!isValidBet(bet))
+        if(!isValidBet(temp))
             placeBet();
-        bet = temp;
-        pot -= bet;
+        else{
+            bet = temp;
+            pot -= bet;
+        }
     }
 
     private static boolean isValidBet(int bet){
@@ -47,7 +54,7 @@ public class Main {
             System.out.println("Can't bet more than you have");
             return false;
         }
-        if(bet < 0){
+        if(bet <= 0){
             System.out.println("Bet must be positive");
             return false;
         }
@@ -55,18 +62,20 @@ public class Main {
     }
 
     private static void play(){
-        if(isBurn()){
+        if(blackjack)
+            return;
+        if(isBurn(true)){
             System.out.println("BURN!");
             burn = true;
             return;
         }
         System.out.println("Enter 1 for hit\nEnter 2 for stand");
         int decision = scanner.nextInt();
-        while(!isValidDecisioin(decision))
+        while(!isValidDecision(decision))
             play();
         switch(decision){
             case 1:
-                hit();
+                hit(true);
                 break;
             case 2:
                 return;
@@ -74,14 +83,21 @@ public class Main {
         play();
     }
 
-    private static boolean isBurn(){
-        return playerResult() > 21;
+    //true == player    false == dealer
+    private static boolean isBurn(boolean player){
+        return calResult(player) > 21;
     }
 
-    private static int playerResult(){
+    //true == player    false == dealer
+    private static int calResult(boolean player){
+        ArrayList<Card> cards;
+        if(player)
+            cards = hand;
+        else
+            cards = dealerHand;
         int sum = 0;
         int aces = 0;
-        for(Card card : hand){
+        for(Card card : cards){
             sum += card.getNumericRank();
             if(card.getRank() == Rank.A)
                 aces++;
@@ -91,5 +107,83 @@ public class Main {
             aces--;
         }
         return sum;
+    }
+
+
+    private static boolean isValidDecision(int dec){
+        return dec == 1 || dec == 2;
+    }
+
+    //true == player    false == dealer
+    private static void hit(boolean player){
+        ArrayList<Card> cards;
+        if(player)
+            cards = hand;
+        else
+            cards = dealerHand;
+        cards.add(deck.get(deckIndex++));
+        cards.get(cards.size()-1).show();
+        printCards();
+    }
+
+    private static void shuffle(){
+        Collections.shuffle(deck);
+    }
+
+    private static void deal(){
+        hand.add(deck.get(deckIndex++)); hand.get(hand.size()-1).show();
+        hand.add(deck.get(deckIndex++)); hand.get(hand.size()-1).show();
+        dealerHand.add(deck.get(deckIndex++));
+        dealerHand.add(deck.get(deckIndex++)); dealerHand.get(dealerHand.size()-1).show();
+        printCards();
+        if(calResult(true) == 21)
+            blackjack = true;
+    }
+
+    private static void blackjack(){
+        blackjack = true;
+    }
+
+    private static void dealerPlay(){
+        if(burn || blackjack)
+            return;
+        while(calResult(false) < 17)
+            hit(false);
+        dealerHand.get(0).show();
+    }
+
+    private static void result(){
+        if(blackjack){
+            System.out.println("BLACKJACK!");
+            pot += 3*bet;
+        }
+        printCards();
+        if(burn || (!dealerBurn && calResult(true) <= calResult(false)))
+            System.out.println("Dealer won");
+        else{
+            System.out.println("You won!");
+            pot += 2*bet;
+        }
+    }
+
+    private static void clear(){
+        hand.clear();
+        dealerHand.clear();
+        bet = 0;
+        burn = false;
+        dealerBurn = false;
+        deckIndex = 0;
+        blackjack = false;
+    }
+
+    private static void printCards(){
+        System.out.print("You: ");
+        for(Card card : hand)
+            System.out.print(card.toString() + "| ");
+        System.out.println();
+        System.out.print("Dealer: ");
+        for(Card card : dealerHand)
+            System.out.print(card.toString() + "| ");
+        System.out.println();
     }
 }
