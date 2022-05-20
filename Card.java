39b4df9709f6c67ce105ecccbf19c9f2e0b8ff0c
benbarak <71935599+benbarak@users.noreply.github.com>
2022-05-20 15:23:24 +0300

    Update Card.java

--- Card.java
+++ Card.java
@@ -2,11 +2,13 @@ public class Card {
     private final Suit suit;
     private final Rank rank;
     private final int numericRank;
+    private boolean shown;
 
-    public Card(Suit suit, Rank rank){
+    public Card(Suit suit, Rank rank, boolean shown){
         this.suit = suit;
         this.rank = rank;
         numericRank = rank.getNumericVal();
+        this.shown = shown;
     }
 
     public int getNumericRank() {
@@ -17,8 +19,18 @@ public class Card {
         return rank;
     }
 
+    public boolean getShown(){
+        return this.shown;
+    }
+
+    public void show(){
+        this.shown = true;
+    }
+
     @Override
     public String toString() {
+        if(!this.shown)
+            return "X";
         if(this.rank == rank.J || this.rank == rank.Q || this.rank == rank.K || this.rank == rank.A)
             return rank.toString() + "." + suit.toString();
         return rank.getNumericVal() + suit.toString();
